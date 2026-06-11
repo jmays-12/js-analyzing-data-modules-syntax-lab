f77f38250e5a7fd733c19ee87bc924b3a2ca2c89
Jordan Mays <jmays12@gmail.com>
2026-06-11 13:42:10 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('datejs');
 
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
 
+    for (const userArray of args) {
+        combinedObject.users.push(...userArray);
+    }
+
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+    return combinedObject;
+};
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+    ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ const {
   require('datejs');
 
   describe('combineUsers function return', () => {
-    const combine = combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"])
+    const combine = combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"]);
     
     
     test('should be an object', () => {
@@ -16,20 +16,20 @@ const {
     });
   });
   describe('combineUsers function', () => {
-    const combine = combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"])
-    const combine2 = combineUsers(["test1"],["test2"])
-    const combine3 = combineUsers([])
+    const combine = combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"]);
+    const combine2 = combineUsers(["test1"],["test2"]);
+    const combine3 = combineUsers([]);
     
     test('should merge all given arrays', () => {
       expect(combine.users).toStrictEqual(["Jim3","Pam5","Dwight77","Michael6","Eleanor22","Chidi202","Jack_jack","Julia_Oreo", "Bill_bore"]);
-      expect(combine2.users).toStrictEqual(["test1","test2"])
-      expect(combine3.users).toStrictEqual([])
+      expect(combine2.users).toStrictEqual(["test1","test2"]);
+      expect(combine3.users).toStrictEqual([]);
     });
 
     test('should contain todays date', () => {
-      expect(combine.merge_date).toBe(Date.today().toString("M/d/yyyy"))
-      expect(combine2.merge_date).toBe(Date.today().toString("M/d/yyyy"))
+      expect(combine.merge_date).toBe(Date.today().toString("M/d/yyyy"));
+      expect(combine2.merge_date).toBe(Date.today().toString("M/d/yyyy"));
     });
     
-  })
+  });
   
